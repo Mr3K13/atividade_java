6f934d36617ed3dd88c9dd09784c36da08cc9461
Mr3K13 <ph154522@gmail.com>
2023-11-30 16:07:18 -0300

    1.2
    1.2

--- Horista.java
+++ Horista.java
@@ -1,26 +1,33 @@
+//Marcus Vinicius Ferraz Teixeira de Mendonça - 01545162 - Sistemas da informação
+//João Gabriel Matias Souza - 01603557 - Engenharia da computação
 package atividade_java;
 
 import javax.swing.JOptionPane;
 
-// Classe derivada Mensalista
+// Classe derivada Horista
 public class Horista extends Funcionario {
 
     private Double salarioHora;
 
     public Double getSalario() {
-        return this.salarioHora;
+
+     return this.salarioHora;
+
     }
 
     // Método para calcular salário total do Horista
     public void setSalario(Double salariohora) {
-        this.salarioHora = 100 + (salariohora * 6.82);
+
+     this.salarioHora = 100 + (salariohora * 6.82);
+
     }
 
     public void realizarTarefa() {
-        JOptionPane.showMessageDialog(null,
-                "Realizando tarefa genérica para o Horista.",
-                "Realizando tarefa genérica.",
-                1);
+
+     JOptionPane.showMessageDialog(null,
+     "Realizando tarefa genérica para o Horista.",
+     "Realizando tarefa genérica.",
+     1);
 
     }
 }
--- Mensalista.java
+++ Mensalista.java
@@ -1,26 +1,33 @@
+//Marcus Vinicius Ferraz Teixeira de Mendonça - 01545162 - Sistemas da informação
+//João Gabriel Matias Souza - 01603557 - Engenharia da computação
 package atividade_java;
 
 import javax.swing.JOptionPane;
 
-// Classe derivada Horista
+// Classe derivada Mensalista
 public class Mensalista extends Funcionario {
 
   private Double salarioMensal;
 
   public Double getSalario() {
-    return this.salarioMensal;
+
+  return this.salarioMensal;
+  
   }
 
   // Método para resgatar salário total fixo
   public void setSalario(Double salariomensal) {
-    this.salarioMensal = salariomensal + 2000;
+
+  this.salarioMensal = salariomensal + 2000;
+
   }
 
   public void realizarTarefa() {
-    JOptionPane.showMessageDialog(null,
-        "Realizando tarefa genérica para o Mensalista.",
-        "Realizando tarefa genérica.",
-        1);
+
+  JOptionPane.showMessageDialog(null,
+   "Realizando tarefa genérica para o Mensalista.",
+   "Realizando tarefa genérica.",
+   1);
 
   }
 }
